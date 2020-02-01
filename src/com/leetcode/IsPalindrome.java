19f33c41418d1f9405a3cd5289bda6d0cc8c7e59
KJM9626 <513339246@qq.com>
2020-02-01 23:35:55 +0800

    leetCode练习

--- src/com/leetcode/IsPalindrome.java
+++ src/com/leetcode/IsPalindrome.java
@@ -24,5 +24,21 @@ package com.leetcode;
  */
 public class IsPalindrome {
 
+	public static boolean isPalindrome(int x) {
+		if(x<0||(x%10==0&&x!=0)) 
+			return false;
+		
+		int result=0;
+		while(x>result) {
+			int n=x%10;
+			result=result*10+n;
+			x/=10;
+		}
+		return x==result||x==result/10;
+	}
 	
+	public static void main(String[] args) {
+		System.out.println(isPalindrome(12));
+		System.out.println(isPalindrome(121));
+	}
 }
